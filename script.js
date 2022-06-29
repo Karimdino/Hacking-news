3d8ad2962f921c767d8ed15f3a8c0be6439fffbc
Murat <abdkurt1982@gmail.com>
2022-06-29 17:11:38 +0200

    akurt_83342

--- script.js
+++ script.js
@@ -2,24 +2,6 @@
  * Allgemein
  * * * * * * * * * * * * * * * * * * * * * * */
 
-
-
-/* * * * * * * * * * * * * * * * * * * * * * *
- * Rene
- * * * * * * * * * * * * * * * * * * * * * * */
-
-
-
-/* * * * * * * * * * * * * * * * * * * * * * *
- * Karim
- * * * * * * * * * * * * * * * * * * * * * * */
-
-
-
-/* * * * * * * * * * * * * * * * * * * * * * *
- * Abdülaziz
- * * * * * * * * * * * * * * * * * * * * * * */
-
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * Die globale Variable "what" notiert, welche Menüinhalt 
  * zuletzt abgerufen wurde, damit beim Suchen, der Suchbegriff 
@@ -27,6 +9,160 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 let what = 0;
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Sobald ein Menüpunkt angeklickt wird, so werden die 
+ * möglichen Formatierungen aller Menüpunkte zurückgesetzt und
+ * die Formatierung der aktuellen Menüpunkt festgesetzt, sodass
+ * jederzeit bekannt ist, auf welchem Content man sich gerade
+ * befindet.
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+function switch_menu_item( id )
+{
+
+    const menuNEW = document.getElementById("new");
+    const menuPAST = document.getElementById("past");
+    const menuCOMMENT = document.getElementById("comment");
+    const menuASK = document.getElementById("ask");
+
+    const menuCURRENT = document.getElementById(id);
+
+    if ( menuNEW && menuPAST && menuCOMMENT && menuASK )
+    {
+
+        menuNEW.style.backgroundColor = "";
+        menuNEW.style.color = "";
+
+        menuPAST.style.backgroundColor = "";
+        menuPAST.style.color = "";
+
+        menuCOMMENT.style.backgroundColor = "";
+        menuCOMMENT.style.color = "";
+
+        menuASK.style.backgroundColor = "";
+        menuASK.style.color = "";
+
+    }
+
+    if ( menuCURRENT )
+    {
+
+        menuCURRENT.style.backgroundColor = "rgba(23, 23, 23, 0.9)";
+        menuCURRENT.style.color = "#ffffff";
+
+    }
+
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * *
+ * Rene
+ * * * * * * * * * * * * * * * * * * * * * * */
+
+// Ladet den Inhalt von "NEW"
+
+function menu_new(filter = "")
+{
+    // Markieren den Tab "NEW"
+    what = 1;
+    // *** //
+    switch_menu_item("new");
+    // *** //
+    grap( "http://hn.algolia.com/api/v1/search_by_date?tags=story" + filter, 
+          function(data) 
+          {
+                for( let peace of data.hits )
+                    addType1( peace );
+          }
+    );
+}
+
+// Ladet den Inhalt von "PAST"
+
+function menu_past(filter = "")
+{
+    // Markieren den Tab "PAST"
+    what = 2;
+    // *** //
+    switch_menu_item("past");
+    // *** //
+    grap( "http://hn.algolia.com/api/v1/search_by_date?tags=story" + filter, 
+          function(data) 
+          {
+                for( let peace of data.hits )
+                    addType1( peace );
+          }
+    );
+}
+
+// Ladet den Inhalt von "COMMENT"
+
+function menu_comment(filter = "")
+{
+    // Markieren den Tab "COMMENT"
+    what = 3;
+    // *** //
+    switch_menu_item("comment");
+    // *** //
+    grap( "http://hn.algolia.com/api/v1/search_by_date?tags=story" + filter, 
+          function(data) 
+          {
+                for( let peace of data.hits )
+                    addType1( peace );
+          }
+    );
+}
+
+// Ladet den Inhalt von "ASK"
+
+function menu_ask(filter = "")
+{
+    // Markieren den Tab "ASK"
+    what = 4;
+    // *** //
+    switch_menu_item("ask");
+    // *** //
+    grap( "http://hn.algolia.com/api/v1/search_by_date?tags=story" + filter, 
+          function(data) 
+          {
+                for( let peace of data.hits )
+                    addType1( peace );
+          }
+    );
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * *
+ * Karim
+ * * * * * * * * * * * * * * * * * * * * * * */
+
+function filterResult( event )
+{
+
+    // Der eingegegebene Wert wird in die Variable
+    // "filter" ausgelesen
+    let filter = "&query=" + event.target.value;
+
+    // Je nach dem im welchem Menüpunkt wir 
+    // uns befinden, wir rufen den betroffenen
+    // Menüfunktion erneut auf und übergeben ihm
+    // den Suchbegriff, um den Inhalt entsprechend
+    // zu filtern.
+    switch ( what )
+    {
+        case 1: menu_new( filter ); break;
+        case 2: menu_past( filter ); break;
+        case 3: menu_comment( filter ); break;
+        case 4: menu_ask( filter ); break;
+    }
+
+    // Der Inhalt des Eingabefelds wird geleert
+    event.target.value = "";
+
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * *
+ * Abdülaziz
+ * * * * * * * * * * * * * * * * * * * * * * */
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * Fügt einen neuen Eintrag in den MAIN-Tag unter BODY-Tag
@@ -80,6 +216,8 @@ function addType2( { url, author, created_at, comment_text } )
  * neuer Inhalt in den MAIN-Tag geladen werden kann und holt 
  * das JSON-Objekt von einer URL ab und leitet es an eine
  * Callback-Funktion weiter.
+ * url -> Die Webseite von dem was ausgelesen wird
+ * cbk -> Ist die Callbackfunktion die das Ergebnis erhält
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 function grap(url, cbk)
